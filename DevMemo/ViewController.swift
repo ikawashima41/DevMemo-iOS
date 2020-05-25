ae1f237c2f1fb8794c3da1e0bb94fc56eba5150d
ikawashima41 <i.kawashima41@gmail.com>
2020-05-26 05:53:58 +0900

    Initial Commit

---
+++ DevMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevMemo
+//
+//  Created by Iichiro Kawashima on 2020/05/26.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DevMemoTests/DevMemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  DevMemoTests.swift
+//  DevMemoTests
+//
+//  Created by Iichiro Kawashima on 2020/05/26.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+import XCTest
+@testable import DevMemo
+
+class DevMemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
