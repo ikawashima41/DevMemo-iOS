ea0b772f48e1f56bde9a0029fb15c3af2ecad966
ikawashima41 <i.kawashima41@gmail.com>
2020-08-10 03:19:20 +0900

    Add stg setting

--- DevMemo/AppDelegate.swift
+++ DevMemo/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        FirebaseApp.configure()
+        setupFirebase()
 
         Messaging.messaging().delegate = self
         if #available(iOS 10.0, *) {
@@ -35,8 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         application.registerForRemoteNotifications()
 
-        CrashReporter.shared.sendEvent(error: CrashError.test)
-        
         return true
     }
 
@@ -52,6 +50,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    private func setupFirebase() {
+        var currentPath: String {
+            let filePath: String?
+            #if DEBUG
+            filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+            #else
+            filePath = Bundle.main.path(forResource: "GoogleService-Info-release", ofType: "plist")
+            #endif
+            return filePath ?? ""
+        }
+
+        guard !currentPath.isEmpty, let fileopts = FirebaseOptions(contentsOfFile: currentPath) else {
+            return
+        }
+
+        #if DEBUG
+        FirebaseConfiguration.shared.setLoggerLevel(.debug)
+        #else
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+        #endif
+
+        FirebaseApp.configure(options: fileopts)
+
+        CrashReporter.shared.sendEvent(error: CrashError.test)
     }
 
     private func application(application: UIApplication,
