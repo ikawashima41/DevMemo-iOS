de74e987e667d9ecc8cacd1a149230c115600357
ikawashima41 <i.kawashima41@gmail.com>
2020-08-10 02:01:17 +0900

    Add registration view

---
+++ DevMemo/Screen/MemoRegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoRegistrationViewController.swift
+//  DevMemo
+//
+//  Created by Iichiro Kawashima on 2020/08/09.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+final class MemoRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
